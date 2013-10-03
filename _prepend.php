5a87666cf4e79ed9c3593c56980f5af540ffd6b1
franck <carnet.franck.paul@gmail.com>
2013-10-03 17:37:32 +0200

    Cope with new favorites system

--- _prepend.php
+++ _prepend.php
@@ -21,39 +21,41 @@ __('myFavs').__('Add favorite capabilities to all plugins');
 
 // Admin mode only
 
-$core->addBehavior('adminDashboardFavs','myFavsDashboardFavs');
+/* Register favorite */
+$core->addBehavior('adminDashboardFavorites',array('adminMyFavs','adminDashboardFavorites'));
 
-function myFavsDashboardFavs($core,$favs)
+class adminMyFavs
 {
-	// Get all activated plugins
-	$mf_plugins = $core->plugins->getModules();
-	if (!empty($mf_plugins)) {
-		foreach ($mf_plugins as $mf_id => $mf_plugin) {
-			if ($mf_id != 'myFavs') {
-				// Only other plugins
-				$mf_root = $core->plugins->moduleRoot($mf_id);
-				// Looks for index.php, mandatory to create a fav on dashboard
-				if (file_exists($mf_root.'/index.php')) {
-					$mf_found = false;
-					// Looks for _admin.php, mandatory to register fav's behaviours (may be, but should not be, in _prepend.php!)
-					if (file_exists($mf_root.'/_admin.php')) {
-						// Looks for 'adminDashboardFavs' string in PHP code of _admin.php
-						$mf_content = file_get_contents($mf_root.'/_admin.php');
-						if (strpos($mf_content,"'adminDashboardFavs'")) {
-							$mf_found = true;
+	public static function adminDashboardFavorites($core,$favs)
+	{
+		// Get all activated plugins
+		$mf_plugins = $core->plugins->getModules();
+		if (!empty($mf_plugins)) {
+			foreach ($mf_plugins as $mf_id => $mf_plugin) {
+				if ($mf_id != 'myFavs') {
+					// Only other plugins
+					$mf_root = $core->plugins->moduleRoot($mf_id);
+					// Looks for index.php, mandatory to create a fav on dashboard
+					if (file_exists($mf_root.'/index.php')) {
+						$mf_found = false;
+						// Looks for _admin.php, mandatory to register fav's behaviours (may be, but should not be, in _prepend.php!)
+						if (file_exists($mf_root.'/_admin.php')) {
+							// Looks for 'adminDashboardFavs' string in PHP code of _admin.php
+							$mf_content = file_get_contents($mf_root.'/_admin.php');
+							if (strpos($mf_content,"'adminDashboardFavs'") || (strpos($mf_content,"'adminDashboardFavorites'"))) {
+								$mf_found = true;
+							}
 						}
-					}
-					if (!$mf_found) {
-						// Add a fav for this plugin
-						$favs[$mf_id] = new ArrayObject(array(
-							$mf_id,
-							$core->plugins->moduleInfo($mf_id,'name'),
-							'plugin.php?p='.$mf_id,
-							'index.php?pf=myFavs/icon.png',
-							'index.php?pf=myFavs/icon-big.png',
-							$core->plugins->moduleInfo($mf_id,'permissions'),
-							null,
-							null));
+						if (!$mf_found) {
+							// Add a fav for this plugin
+							$favs->register($mf_id, array(
+								'title' => __($core->plugins->moduleInfo($mf_id,'name')),
+								'url' => 'plugin.php?p='.$mf_id,
+								'small-icon' => 'index.php?pf=myFavs/icon.png',
+								'large-icon' => 'index.php?pf=myFavs/icon-big.png',
+								'permissions' => $core->plugins->moduleInfo($mf_id,'permissions')
+							));
+						}
 					}
 				}
 			}
