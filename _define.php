1e50fce966e0fa667dcac5bb15960aa170b68e3a
franck <carnet.franck.paul@gmail.com>
2025-09-22 10:29:17 +0200

    New release 6.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'myFavs',
     'Add favorite capabilities to all plugins',
     'Franck Paul',
-    '6.0',
+    '6.1',
     [
-        'date'     => '2025-09-07T15:54:39+0200',
+        'date'     => '2025-09-22T10:29:17+0200',
         'requires' => [['core', '2.36']],
         'type'     => 'plugin',
         'priority' => 999_999_999,
